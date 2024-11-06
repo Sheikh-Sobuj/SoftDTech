76c1b328ef39baa4935fe53521ab7d82c2a405e4
Sheikh-Sobuj <sheikh.sobuj95@gmail.com>
2024-11-06 22:00:19 +0600

    all component

---
+++ src/app/team-single/team-single.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-single',
+  templateUrl: './team-single.component.html',
+  styleUrls: ['./team-single.component.scss']
+})
+export class TeamSingleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
